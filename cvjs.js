0bbee866e900eb9bffd3bcf1b4a449e7160af274
Facugrimaut <123128668+Facugrimaut@users.noreply.github.com>
2023-01-19 20:25:28 -0300

    Add files via upload
    examen integrador curso ticmas

---
+++ cvjs.js
@@ -0,0 +1,19 @@
+document.getElementById('botonContac').addEventListener('click', function() {
+  document.getElementById("EAdress").removeAttribute("hidden");
+})
+
+document.getElementById('botonbirthday').addEventListener('click', function() {
+  document.getElementById("Birthday").removeAttribute("hidden");
+})
+
+document.getElementById('botonaddress').addEventListener('click', function() {
+  document.getElementById("Adress").removeAttribute("hidden");
+})
+
+document.getElementById('botonPhone').addEventListener('click', function() {
+  document.getElementById("Phone").removeAttribute("hidden");
+})
+
+document.getElementById('botonpassword').addEventListener('click', function() {
+  document.getElementById("Password").removeAttribute("hidden");
+});
